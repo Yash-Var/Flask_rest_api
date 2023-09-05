e7b7d451a7bd8f3976764c8474bcfecc4817bb65
Yash-Var <yashvarshney7011@gmail.com>
2023-09-05 13:43:46 +0530

    update the code

--- app.py
+++ app.py
@@ -1,89 +1,102 @@
-from flask import Flask, jsonify, request  
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from bson import ObjectId
-app = Flask(__name__)
+from flask import Blueprint
 
-app.config['MONGO_URI'] = 'mongodb+srv://varshney:Sj55888@cluster0.jqzobx2.mongodb.net/JWT_AUTH?retryWrites=true&w=majority'
-client = MongoClient(app.config['MONGO_URI'])
-db = client['JWT_AUTH'] 
+class UserAPI:
+    def __init__(self, app, db):
+        self.app = app
+        self.db = db
 
-@app.route('/users')
-def get_users():
-    users_cursor = db.users.find()
-    users_list = [user for user in users_cursor]
+
+
+    def configure_routes(self):
+        user_bp = Blueprint('user_bp', __name__)
+
+        @user_bp.route('/users', methods=['GET'])
+        def get_users():
+            users_cursor = self.db.users.find()
+            users_list = [user for user in users_cursor]
+
+            for user in users_list:
+                user['_id'] = str(user['_id'])
+
+            return jsonify({'users': users_list})
+
+
+
+        @user_bp.route('/users/<user_id>', methods=['GET'])
+        def get_user(user_id):
+            user = self.db.users.find_one({'_id': ObjectId(user_id)})
+
+            if user:
+                user['_id'] = str(user['_id'])
+                return jsonify({'user': user})
+            else:
+                return jsonify({'message': 'User not found'}), 404
+
+
+
+        @user_bp.route('/users', methods=['POST'])
+        def create_user():
+            data = request.json
+
+            if 'name' in data and 'email' in data and 'password' in data:
+                user_data = {
+                    'name': data['name'],
+                    'email': data['email'],
+                    'password': data['password']
+                }
+                user_id = self.db.users.insert_one(user_data).inserted_id
+                return jsonify({'message': 'User created', '_id': str(user_id)}), 201
+            else:
+                return jsonify({'message': 'Missing name, email, or password'}), 400
+
+
+
+        @user_bp.route('/users/<user_id>', methods=['PUT'])
+        def update_user(user_id):
+            data = request.json
+
+            if 'username' in data or 'email' in data:
+                result = self.db.users.update_one({'_id': ObjectId(user_id)}, {'$set': data})
+
+                if result.modified_count > 0:
+                    return jsonify({'message': 'User updated'}), 200
+                else:
+                    return jsonify({'message': 'User not found'}), 404
+            else:
+                return jsonify({'message': 'No data to update'}), 400
+
+
+
+        @user_bp.route('/users/<user_id>', methods=['DELETE'])
+        def delete_user(user_id):
+            result = self.db.users.delete_one({'_id': ObjectId(user_id)})
+
+            if result.deleted_count > 0:
+                return jsonify({'message': 'User deleted'}), 200
+            else:
+                return jsonify({'message': 'User not found'}), 404
+
+        
+        self.app.register_blueprint(user_bp)
+
+        
+
+def create_app():
+    app = Flask(__name__)
 
     
-    for user in users_list:
-        user['_id'] = str(user['_id'])
+    app.config['MONGO_URI'] = 'mongodb+srv://varshney:Sj55888@cluster0.jqzobx2.mongodb.net/JWT_AUTH?retryWrites=true&w=majority'
+    client = MongoClient(app.config['MONGO_URI'])
+    db = client['JWT_AUTH']
 
-    return jsonify({'users': users_list})
+    user_api = UserAPI(app, db)
+    user_api.configure_routes()
 
-
-
-@app.route('/users/<string:id>')
-def get_user(id):
-    try:
-        print("Received ID:", id)  
-        user_id = ObjectId(id)
-        print("Converted to ObjectId:", user_id)  
-        
-        user = db.users.find_one({'_id': user_id})
-
-        if user:
-            
-            user['_id'] = str(user['_id'])
-            return jsonify({'user': user})
-        else:
-            return jsonify({'message': 'User not found'}), 404
-    except Exception as e:
-        return jsonify({'message': str(e)}), 400 
-
-
- 
-
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.json  
-    user = {
-        'name': data['name'],
-        'email': data['email'],
-        'password': data['password']
-    }
-    db.users.insert_one(user)
-    return jsonify({'message': 'User created'})
-
-
-
-@app.route('/users/<id>', methods=['PUT'])
-def update_user(id):
-    try:
-        data = request.json
-        user_id = ObjectId(id)  
-        update_result = db.users.update_one({'_id': user_id}, {'$set': data})
-
-        if update_result.modified_count > 0:
-            return jsonify({'message': 'User updated'})
-        else:
-            return jsonify({'message': 'User not found'}), 404
-    except Exception as e:
-        return jsonify({'message': str(e)}), 400
-
-
-
-@app.route('/users/<id>', methods=['DELETE'])
-def delete_user(id):
-    try:
-        user_id = ObjectId(id) 
-        delete_result = db.users.delete_one({'_id': user_id})
-
-        if delete_result.deleted_count > 0:
-            return jsonify({'message': 'User deleted'})
-        else:
-            return jsonify({'message': 'User not found'}), 404
-    except Exception as e:
-        if isinstance(e, Timestamp):
-            return jsonify({'message': 'User deleted'}), 200  
-        return jsonify({'message': str(e)}), 400
+    return app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app = create_app()
+    app.run(debug=True)
